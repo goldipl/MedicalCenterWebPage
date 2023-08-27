755e28f9d1f83750a653cede2613dfce2b1cbbe0
goldipl <mgodlewski92@gmail.com>
2023-08-27 11:11:42 +0200

    fix scroll into section bug

---
+++ js/scrollintoview.js
@@ -0,0 +1,11 @@
+const scrollIntoAboutView = () => {
+    document.querySelector('#about').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+const scrollIntoSpaView = () => {
+    document.querySelector('#spa').scrollIntoView({
+        behavior: 'smooth'
+    });
+}
